3ab424987a4570816e2fffd9d23390cdfa70b9be
John Livingston <jmlivingston@gmail.com>
2021-06-28 16:36:41 -0700

    Remove AWS_S3_SIGNED_URL

--- src/utility/CONSTANTS.js
+++ src/utility/CONSTANTS.js
@@ -1,7 +1,6 @@
 const ENV_KEY = 'B'
 const AWS_ACCESS_KEY_ID = process.env[`AWS_ACCESS_KEY_ID_${ENV_KEY}`]
 const AWS_S3_BUCKET = process.env[`AWS_S3_BUCKET_${ENV_KEY}`]
-const AWS_S3_SIGNED_URL = process.env[`AWS_S3_SIGNED_URL_${ENV_KEY}`]
 const AWS_SECRET_ACCESS_KEY = process.env[`AWS_SECRET_ACCESS_KEY_${ENV_KEY}`]
 const AWS_SIGNATURE_VERSION = 'v4'
 const ROUTES = Object.freeze({
@@ -12,7 +11,6 @@ const ROUTES = Object.freeze({
 export {
   AWS_ACCESS_KEY_ID,
   AWS_S3_BUCKET,
-  AWS_S3_SIGNED_URL,
   AWS_SECRET_ACCESS_KEY,
   AWS_SIGNATURE_VERSION,
   ENV_KEY,
