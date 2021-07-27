6e4c7107d96fb169b11bf5e7d32880891d9cb74f
John Livingston <jmlivingston@gmail.com>
2021-07-27 07:59:46 -0700

    Update payment

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -1,5 +1,6 @@
 import { useMachine } from '@xstate/react';
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import { API_ROUTES, NEUVEI_API_CHALLENGE_SIMULATOR } from '../../config/CONSTANTS';
 import Input from '../common/Input';
 import { defaultFormState, getTestCardHolders, getTestCards, strings } from './paymentConfig';
@@ -13,84 +14,121 @@ function Payment({ isInspectorOnly }) {
   const [state, send] = useMachine(paymentMachine, { devTools: true });
   const [formState, setFormState] = useState({ ...defaultFormState, cardNumber: activeCard.value });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [flowState, setFlowState] = useState('init');
+  const [flowState, setFlowState] = useState('unsubmitted');
   const [error, setError] = useState(false);
   const [paymentResponseState, setPaymentResponseState] = useState({});
   const [isFallback, setIsFallback] = useState(false);
+  const {
+    query: { challenge },
+  } = useRouter();
+  useEffect(() => {
+    (async () => {
+      if (challenge === 'accepted') {
+        const paymentDetails = localStorage.getItem('paymentDetails');
+        if (paymentDetails) {
+          setFlowState('liabilityShift');
+          await callPayment(JSON.parse(paymentDetails));
+          localStorage.removeItem('paymentDetails');
+        }
+      }
+    })();
+    setCardHolder(activeCard.value);
+  }, []);
 
-  const onSubmit = async (event) => {
+  const callPayment = async (body) => {
     setError();
     setIsSubmitting(true);
-    event.preventDefault();
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    const isInit = flowState === 'init';
-    const isChallenge = flowState === 'challenge';
-    let paymentResponseJson;
-    if (isChallenge) {
-      const url = NEUVEI_API_CHALLENGE_SIMULATOR({
-        acsUrl: paymentResponseState.paymentOption.card.threeD.acsUrl,
-        cReq: paymentResponseState.paymentOption.card.threeD.cReq,
-      });
-      console.log(url);
-      setIsSubmitting(false);
-    } else {
-      const body = isInit
-        ? {
-            amount: formState.amount,
-            cardHolderName: formState.cardHolderName,
-            cardNumber: formState.cardNumber,
-            currency: formState.currency,
-            CVV: formState.CVV,
-            expirationMonth: formState.expirationMonth,
-            expirationYear: formState.expirationYear,
-            isInit,
-            isFallback,
-            isLiabilityShift: false,
-          }
-        : {
-            amount: formState.amount,
-            cardHolderName: formState.cardHolderName,
-            cardNumber: formState.cardNumber,
-            currency: formState.currency,
-            CVV: formState.CVV,
-            expirationMonth: formState.expirationMonth,
-            expirationYear: formState.expirationYear,
-            clientRequestId: paymentResponseState.clientRequestId,
-            sessionToken: paymentResponseState.sessionToken,
-            clientRequestId: paymentResponseState.clientRequestId,
-            relatedTransactionId: paymentResponseState.transactionId,
-            sessionToken: paymentResponseState.sessionToken,
-            paResponse: isChallenge ? undefined : '{{PaResponse}}', //HACK comes from somewhere?
-            relatedTransactionId: paymentResponseState.transactionId,
-            isInit,
-            isFallback,
-            isLiabilityShift: true,
-          };
-      const paymentResponse = await fetch(API_ROUTES.PAYMENT, {
-        method: 'POST',
-        body: JSON.stringify(body),
-        headers,
-      });
-      paymentResponseJson = await paymentResponse.json();
-      setPaymentResponseState({ ...body, ...paymentResponseJson });
-      setIsSubmitting(false);
-      setError(paymentResponseJson.transactionStatus + ' - ' + paymentResponseJson.status);
-    }
+    const paymentResponse = await fetch(API_ROUTES.PAYMENT, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers,
+    });
+    let paymentResponseJson = await paymentResponse.json();
+    setPaymentResponseState({ ...body, ...paymentResponseJson });
+    setIsSubmitting(false);
+    setError(paymentResponseJson.transactionStatus); // + ' - ' + paymentResponseJson.status);
     switch (flowState) {
-      case 'init':
+      case 'unsubmitted':
         if (paymentResponseJson.transactionStatus === 'APPROVED' && !isFallback) {
-          setFlowState('init');
+          // Frictionless
+          setFlowState('unsubmitted');
         } else {
           setFlowState(paymentResponseJson.transactionStatus === 'REDIRECT' ? 'challenge' : 'liabilityShift');
         }
         break;
       case 'liabilityShift':
-        setFlowState('init');
+        setFlowState('unsubmitted');
         break;
       case 'challenge':
         setFlowState('liabilityShift');
         break;
+    }
+    if (paymentResponseJson.transactionStatus === 'REDIRECT') {
+      localStorage.setItem(
+        'paymentDetails',
+        JSON.stringify({
+          amount: formState.amount,
+          cardHolderName: formState.cardHolderName,
+          cardNumber: formState.cardNumber,
+          clientRequestId: paymentResponseJson.clientRequestId,
+          currency: formState.currency,
+          CVV: formState.CVV,
+          expirationMonth: formState.expirationMonth,
+          expirationYear: formState.expirationYear,
+          isFallback: paymentResponseJson.isFallback,
+          isInit: paymentResponseJson.isInit,
+          isLiabilityShift: true,
+          // paResponse: isChallenge ? undefined : '{{PaResponse}}', //HACK comes from somewhere?
+          relatedTransactionId: paymentResponseJson.transactionId,
+          sessionToken: paymentResponseJson.sessionToken,
+        })
+      );
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event?.preventDefault();
+    const isInit = flowState === 'unsubmitted';
+    const isChallenge = flowState === 'challenge';
+    if (isChallenge) {
+    } else {
+      let body;
+      if (isInit) {
+        body = {
+          amount: formState.amount,
+          cardHolderName: formState.cardHolderName,
+          cardNumber: formState.cardNumber,
+          currency: formState.currency,
+          CVV: formState.CVV,
+          expirationMonth: formState.expirationMonth,
+          expirationYear: formState.expirationYear,
+          isFallback,
+          isInit,
+          isLiabilityShift: false,
+        };
+        body.notificationURL = `http://localhost:9000/pages/payment?challenge=accepted`;
+      } else {
+        body = {
+          amount: formState.amount,
+          cardHolderName: formState.cardHolderName,
+          cardNumber: formState.cardNumber,
+          clientRequestId: paymentResponseState.clientRequestId,
+          clientRequestId: paymentResponseState.clientRequestId,
+          currency: formState.currency,
+          CVV: formState.CVV,
+          expirationMonth: formState.expirationMonth,
+          expirationYear: formState.expirationYear,
+          isFallback,
+          isInit,
+          isLiabilityShift: true,
+          paResponse: isChallenge ? undefined : '{{PaResponse}}', //HACK comes from somewhere?
+          relatedTransactionId: paymentResponseState.transactionId,
+          sessionToken: paymentResponseState.sessionToken,
+        };
+      }
+      callPayment(body);
     }
   };
 
@@ -101,6 +139,20 @@ function Payment({ isInspectorOnly }) {
     });
   };
 
+  const setCardHolder = (card) => {
+    switch (card) {
+      case '4000020951595032':
+        setActiveUser(users['CL-BRW1']);
+        break;
+      case '4000027891380961':
+        setActiveUser(users['FL-BRW1']);
+        break;
+      case '4012001037141112':
+        setActiveUser(users['John Smith']);
+        break;
+    }
+  };
+
   const onCardChange = (event) => {
     const activeCard = cards[event.target.value];
     setActiveCard(activeCard);
@@ -108,6 +160,7 @@ function Payment({ isInspectorOnly }) {
       ...formState,
       cardNumber: activeCard.value,
     });
+    setCardHolder(activeCard.value);
   };
 
   const onCardHolderChange = (event) => {
@@ -119,6 +172,13 @@ function Payment({ isInspectorOnly }) {
     });
     setIsFallback(activeUser.notes.includes('Fallback'));
   };
+
+  const challengeUrl = paymentResponseState?.paymentOption?.card?.threeD?.acsUrl
+    ? NEUVEI_API_CHALLENGE_SIMULATOR({
+        acsUrl: paymentResponseState.paymentOption.card.threeD.acsUrl,
+        cReq: paymentResponseState.paymentOption.card.threeD.cReq,
+      })
+    : null;
 
   return (
     <>
@@ -227,10 +287,11 @@ function Payment({ isInspectorOnly }) {
                 {strings.submit}
               </button>
             </div>
+            <a href={challengeUrl}>{challengeUrl}</a>
           </form>
           <h1>{flowState}</h1>
           {error && (
-            <div className={`alert alert-${error === 'ERROR' ? 'danger' : 'success'} mt-3`} role="alert">
+            <div className={`alert alert-${error.includes('ERROR') ? 'danger' : 'success'} mt-3`} role="alert">
               {error}
             </div>
           )}
